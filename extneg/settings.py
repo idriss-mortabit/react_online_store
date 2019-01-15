c09d7e8d023d1ba1063e686415a78c909b5390ec
idriss <idrissste@gmail.com>
2019-01-16 02:14:42 +0530

    master branch

--- extneg/settings.py
+++ extneg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'b*q#81!6$6h570m19n_%c$%y@8ehy2je^z4n6cg!ordlirc#6*'
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 
-                 'www.extneg.com',
+                 'www.\extneg.com',
                  'extneg.com']
 
 
