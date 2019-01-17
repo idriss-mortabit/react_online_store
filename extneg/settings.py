6a81f1357ea885eec033e647f35d69afe79f84fc
Idriss Mortabit <32003521+idriss-mortabit@users.noreply.github.com>
2019-01-17 12:04:52 +0530

    Update settings.py

--- extneg/settings.py
+++ extneg/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 
                  'www.extneg.com',
-                 'extneg.com']
+                 'extneg.com',
+		 'extneg.herokuapp.com']
 
 
 # Application definition
