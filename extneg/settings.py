cfc3e672b51d857367df529c51eac53ef492c8db
Idriss Mortabit <32003521+idriss-mortabit@users.noreply.github.com>
2019-01-31 12:18:28 +0530

    Update settings.py

--- extneg/settings.py
+++ extneg/settings.py
@@ -85,14 +85,22 @@ WSGI_APPLICATION = 'extneg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'knowjrrt_extneg_db',
+        'USER': 'knowjrrt_extneg',
+        'PASSWORD': 'jztN9cnvsbkv',
+        'HOST': 'https://server233.web-hosting.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '2083',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
